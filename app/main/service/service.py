e0282f518ca456005c1176be3e06a8d419dd15ab
Glen Escuadra <glen.escuadra@g.msuiit.edu.ph>
2019-03-13 23:28:37 +0800

    Updated models, services, and tests
    Added BlacklistTokens for models, save_token for services.
    Created tests for authentication

--- app/main/service/service.py
+++ app/main/service/service.py
@@ -2,7 +2,7 @@ import uuid
 import datetime
 
 from app.main import db
-from app.main.model.models import User
+from app.main.model.models import User, BlacklistToken
 
 
 def save_new_user(data):
@@ -40,3 +40,22 @@ def get_a_user(public_id):
 def save_changes(data):
     db.session.add(data)
     db.session.commit()
+
+
+def save_token(token):
+    blacklist_token = BlacklistToken(token=token)
+    try:
+        # insert the token
+        db.session.add(blacklist_token)
+        db.session.commit()
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully logged out.'
+        }
+        return response_object, 200
+    except Exception as e:
+        response_object = {
+            'status': 'fail',
+            'message': e
+        }
+        return response_object, 200
