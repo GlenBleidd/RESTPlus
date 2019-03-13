ea3f6872e648a861c91561fc6a41ba7dd31920e0
Glen Escuadra <glen.escuadra@g.msuiit.edu.ph>
2019-03-13 23:52:27 +0800

    Created authentication endpoints for login and logout
    Updated controller, service, dto

--- app/main/controller/controller.py
+++ app/main/controller/controller.py
@@ -1,11 +1,15 @@
 from flask import request
 from flask_restplus import Resource
 
-from ..util.dto import UserDto
+from app.main.service.service import Auth
+
+from ..util.dto import UserDto, AuthDto
 from ..service.service import save_new_user, get_all_users, get_a_user
 
 api = UserDto.api
+api_auth = AuthDto.api
 _user = UserDto.user
+user_auth = AuthDto.user_auth
 
 
 @api.route('/')
@@ -38,3 +42,28 @@ class User(Resource):
             api.abort(404)
         else:
             return user
+
+
+@api_auth.route('/login')
+class UserLogin(Resource):
+    """
+        User Login Resource
+    """
+    @api.doc('user login')
+    @api.expect(user_auth, validate=True)
+    def post(self):
+        # get the post data
+        post_data = request.json
+        return Auth.login_user(data=post_data)
+
+
+@api_auth.route('/logout')
+class LogoutAPI(Resource):
+    """
+    Logout Resource
+    """
+    @api.doc('logout a user')
+    def post(self):
+        # get auth token
+        auth_header = request.headers.get('Authorization')
+        return Auth.logout_user(data=auth_header)
--- app/main/service/service.py
+++ app/main/service/service.py
@@ -49,13 +49,68 @@ def save_token(token):
         db.session.add(blacklist_token)
         db.session.commit()
         response_object = {
-            'status': 'success',
+            'status': 'Success!',
             'message': 'Successfully logged out.'
         }
         return response_object, 200
     except Exception as e:
         response_object = {
-            'status': 'fail',
+            'status': 'Failed!',
             'message': e
         }
         return response_object, 200
+
+
+class Auth:
+
+    @staticmethod
+    def login_user(data):
+        try:
+            # fetch the user data
+            user = User.query.filter_by(email=data.get('email')).first()
+            if user and user.check_password(data.get('password')):
+                auth_token = user.encode_auth_token(user.id)
+                if auth_token:
+                    response_object = {
+                        'status': 'Success!',
+                        'message': 'Successfully logged in.',
+                        'Authorization': auth_token.decode()
+                    }
+                    return response_object, 200
+            else:
+                response_object = {
+                    'status': 'Failed!',
+                    'message': 'email or password does not match.'
+                }
+                return response_object, 401
+
+        except Exception as e:
+            print(e)
+            response_object = {
+                'status': 'Failed!',
+                'message': 'Please try again.'
+            }
+            return response_object, 500
+
+    @staticmethod
+    def logout_user(data):
+        if data:
+            auth_token = data.split(" ")[1]
+        else:
+            auth_token = ''
+        if auth_token:
+            resp = User.decode_auth_token(auth_token)
+            if not isinstance(resp, str):
+                return save_token(token=auth_token)
+            else:
+                response_object = {
+                    'status': 'fail',
+                    'message': resp
+                }
+                return response_object, 401
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Provide a valid auth token.'
+            }
+            return response_object, 403
